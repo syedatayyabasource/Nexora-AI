53b01ad1115b624e2271e6d239e119b3ad235717
Tayyaba Zahra <syedatayyaba2005@gmail.com>
2026-08-28 12:45:04 +0500

    Fix merge conflicts and production build

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import {StrictMode} from "react"; import {createRoot} from "react-dom/client"; import {BrowserRouter} from "react-router-dom"; import App from "./App"; import "./index.css";
-createRoot(document.getElementById("root")).render(<StrictMode><BrowserRouter><App/></BrowserRouter></StrictMode>);
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -15,4 +11,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </React.StrictMode>
 );
->>>>>>> dcca521e1fb2836113d51c8b4de9cc837f07b23b
